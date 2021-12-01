2009803a91ad3d960ed786515ce6a1f0f4bf9dfa
William Hicks <whicks@nvidia.com>
2021-12-01 10:00:05 -0500

    Remove unnecessary capture from lambda

--- cpp/include/rapids_triton/memory/detail/gpu_only/owned_device_buffer.hpp
+++ cpp/include/rapids_triton/memory/detail/gpu_only/owned_device_buffer.hpp
@@ -30,7 +30,7 @@ template<typename T>
 struct owned_device_buffer<T, true> {
   using non_const_T = std::remove_const_t<T>;
   owned_device_buffer(device_id_t device_id, std::size_t size, cudaStream_t stream)
-    : data_{[this, &device_id, &size, &stream]() {
+    : data_{[&device_id, &size, &stream]() {
       auto device_context = device_setter{device_id};
       return rmm::device_buffer{size * sizeof(T), rmm::cuda_stream_view{stream}};
     }()}
