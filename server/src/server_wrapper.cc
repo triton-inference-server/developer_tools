5c27c7ee9f2e0c809a223b71dc92bd52afb778cc
krishung5 <krish@nvidia.com>
2022-08-24 12:17:03 -0700

    Fix up

--- server/src/server_wrapper.cc
+++ server/src/server_wrapper.cc
@@ -24,7 +24,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "server_api.h"
+#include "server_wrapper.h"
 #include <stdlib.h>
 #include <iostream>
 #include <sstream>
