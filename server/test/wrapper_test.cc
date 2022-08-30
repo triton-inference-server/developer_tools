ae82be1083a937fe8e9ff5477c5556639744e96d
krishung5 <krish@nvidia.com>
2022-08-30 15:33:01 -0700

    Make changes for testing

--- server/test/wrapper_test.cc
+++ server/test/wrapper_test.cc
@@ -25,9 +25,9 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "gtest/gtest.h"
 
+#include <exception>
 #include "server_wrapper.h"
 #include "triton/core/tritonserver.h"
-#include <exception>
 
 namespace tsw = triton::server::wrapper;
 
@@ -35,7 +35,7 @@ namespace {
 
 TEST(TritonServer, SanityCheck)
 {
-  // Sanity check that proper 'libtritonserver->so' is used
+  // Sanity check that proper 'libtritonserver.so' is used
   uint32_t major = 0, minor = 0;
   auto err = TRITONSERVER_ApiVersion(&major, &minor);
   ASSERT_TRUE(err == nullptr) << "Unexpected error from API version call";
@@ -47,19 +47,24 @@ TEST(TritonServer, StartInvalidRepository)
 {
   // Run server with invalid model repository
   try {
-    tsw::TritonServer::Create(tsw::ServerOptions({"/invalid_model_repository"}));
-  } catch (std::exception& ex) {
-    ASSERT_STREQ(ex.what(), "Internal-failed to stat file /invalid_model_repository\n");
-  } catch (...) {
+    tsw::TritonServer::Create(
+        tsw::ServerOptions({"/invalid_model_repository"}));
+  }
+  catch (std::exception& ex) {
+    ASSERT_STREQ(
+        ex.what(), "Internal-failed to stat file /invalid_model_repository\n");
+  }
+  catch (...) {
     ASSERT_NO_THROW(throw);
   }
 }
 
 class TritonServerTest : public ::testing::Test {
  protected:
-  TritonServerTest()
-    : options_({"./models"}) {
-    options_.logging_ = tsw::LoggingOptions(false, false, false, false, tsw::Wrapper_LogFormat::LOG_DEFAULT, "");
+  TritonServerTest() : options_({"./models"})
+  {
+    options_.logging_ = tsw::LoggingOptions(
+        false, false, false, false, tsw::LogFormat::LOG_DEFAULT, "");
   }
 
   tsw::ServerOptions options_;
@@ -75,7 +80,8 @@ TEST_F(TritonServerTest, StartNone)
     ASSERT_EQ(loaded_models.size(), 2);
     ASSERT_NE(loaded_models.find("add_sub"), loaded_models.end());
     ASSERT_NE(loaded_models.find("add_sub_str"), loaded_models.end());
-  } catch (...) {
+  }
+  catch (...) {
     ASSERT_NO_THROW(throw);
   }
 }
@@ -87,7 +93,8 @@ TEST_F(TritonServerTest, NoneLoadUnload)
     auto server = tsw::TritonServer::Create(options_);
     ASSERT_FALSE(server->LoadModel("add_sub").IsOk());
     ASSERT_FALSE(server->UnloadModel("add_sub").IsOk());
-  } catch (...) {
+  }
+  catch (...) {
     ASSERT_NO_THROW(throw);
   }
 }
@@ -107,7 +114,8 @@ TEST_F(TritonServerTest, Explicit)
     ASSERT_TRUE(server->UnloadModel("add_sub").IsOk());
     ASSERT_TRUE(server->LoadedModels(&loaded_models).IsOk());
     ASSERT_EQ(loaded_models.size(), 0);
-  } catch (...) {
+  }
+  catch (...) {
     ASSERT_NO_THROW(throw);
   }
 }
@@ -124,35 +132,39 @@ TEST_F(TritonServerTest, InferMinimal)
     }
     std::vector<tsw::Tensor> inputs;
     for (const auto& name : std::vector<std::string>{"INPUT0", "INPUT1"}) {
-      inputs.emplace_back(tsw::Tensor(name, reinterpret_cast<char*>(input_data.data()),
-      input_data.size() * sizeof(int32_t), tsw::Wrapper_DataType::INT32, {16},
-      tsw::Wrapper_MemoryType::CPU, 0));
+      inputs.emplace_back(tsw::Tensor(
+          name, reinterpret_cast<char*>(input_data.data()),
+          input_data.size() * sizeof(int32_t), tsw::DataType::INT32, {16},
+          tsw::MemoryType::CPU, 0));
     }
-    
-    auto request = tsw::InferRequest(tsw::InferOptions("add_sub"));
+
+    auto request = tsw::InferRequest::Create(tsw::InferOptions("add_sub"));
     for (const auto& input : inputs) {
-      ASSERT_TRUE((err = request.AddInput(input)).IsOk()) << err.Message();
+      ASSERT_TRUE((err = request->AddInput(input)).IsOk()) << err.Message();
     }
-    std::future<tsw::InferResult> result_future;
-    ASSERT_TRUE((err = server->AsyncInfer(&result_future, request)).IsOk()) << err.Message();
+    std::future<std::unique_ptr<tsw::InferResult>> result_future;
+    ASSERT_TRUE((err = server->AsyncInfer(&result_future, *request)).IsOk())
+        << err.Message();
     auto result = result_future.get();
-    ASSERT_FALSE(result.HasError()) << result.ErrorMsg();
+    ASSERT_FALSE(result->HasError()) << result->ErrorMsg();
 
     // Check result metadata
-    ASSERT_EQ(result.ModelName() , "add_sub");
-    ASSERT_EQ(result.ModelVersion(), "1");
-    ASSERT_EQ(result.Id(), "");
+    ASSERT_EQ(result->ModelName(), "add_sub");
+    ASSERT_EQ(result->ModelVersion(), "1");
+    ASSERT_EQ(result->Id(), "");
 
     // OUTPUT0 -> sum
     {
       std::string out_name("OUTPUT0");
       tsw::Tensor out(out_name);
-      ASSERT_TRUE((err = result.Output(&out)).IsOk()) << err.Message();
+      ASSERT_TRUE((err = result->Output(&out)).IsOk()) << err.Message();
       ASSERT_EQ(out.shape_, std::vector<int64_t>{16});
-      ASSERT_EQ(out.data_type_, tsw::Wrapper_DataType::INT32);
+      ASSERT_EQ(out.data_type_, tsw::DataType::INT32);
       ASSERT_EQ(out.byte_size_, (input_data.size() * sizeof(int32_t)));
       for (size_t i = 0; i < input_data.size(); ++i) {
-        EXPECT_EQ(reinterpret_cast<const int32_t*>(out.buffer_)[i], (2 * input_data[i]));
+        EXPECT_EQ(
+            reinterpret_cast<const int32_t*>(out.buffer_)[i],
+            (2 * input_data[i]));
       }
     }
 
@@ -160,15 +172,16 @@ TEST_F(TritonServerTest, InferMinimal)
     {
       std::string out_name("OUTPUT1");
       tsw::Tensor out(out_name);
-      ASSERT_TRUE((err = result.Output(&out)).IsOk()) << err.Message();
+      ASSERT_TRUE((err = result->Output(&out)).IsOk()) << err.Message();
       ASSERT_EQ(out.shape_, std::vector<int64_t>{16});
-      ASSERT_EQ(out.data_type_, tsw::Wrapper_DataType::INT32);
+      ASSERT_EQ(out.data_type_, tsw::DataType::INT32);
       ASSERT_EQ(out.byte_size_, (input_data.size() * sizeof(int32_t)));
       for (size_t i = 0; i < input_data.size(); ++i) {
         EXPECT_EQ(reinterpret_cast<const int32_t*>(out.buffer_)[i], 0);
       }
     }
-  } catch (...) {
+  }
+  catch (...) {
     ASSERT_NO_THROW(throw);
   }
 }
@@ -186,32 +199,38 @@ TEST_F(TritonServerTest, InferString)
       input_data_str.emplace_back(std::to_string(input_data.back()));
     }
 
-    auto request = tsw::InferRequest(tsw::InferOptions("add_sub_str"));
+    auto request = tsw::InferRequest::Create(tsw::InferOptions("add_sub_str"));
     for (const auto& name : std::vector<std::string>{"INPUT0", "INPUT1"}) {
-      ASSERT_TRUE((err = request.AddInput(name, input_data_str.begin(), input_data_str.end(), {16})).IsOk()) << err.Message();
+      ASSERT_TRUE(
+          (err = request->AddInput(
+               name, input_data_str.begin(), input_data_str.end(), {16}))
+              .IsOk())
+          << err.Message();
     }
-    
-    std::future<tsw::InferResult> result_future;
-    ASSERT_TRUE((err = server->AsyncInfer(&result_future, request)).IsOk()) << err.Message();
+
+    std::future<std::unique_ptr<tsw::InferResult>> result_future;
+    ASSERT_TRUE((err = server->AsyncInfer(&result_future, *request)).IsOk())
+        << err.Message();
     auto result = result_future.get();
-    ASSERT_FALSE(result.HasError()) << result.ErrorMsg();
+    ASSERT_FALSE(result->HasError()) << result->ErrorMsg();
 
     // Check result metadata
-    ASSERT_EQ(result.ModelName() , "add_sub_str");
-    ASSERT_EQ(result.ModelVersion(), "1");
-    ASSERT_EQ(result.Id(), "");
+    ASSERT_EQ(result->ModelName(), "add_sub_str");
+    ASSERT_EQ(result->ModelVersion(), "1");
+    ASSERT_EQ(result->Id(), "");
 
     std::vector<std::string> out_str;
     std::vector<int64_t> shape;
-    tsw::Wrapper_DataType datatype;
+    tsw::DataType datatype;
     // OUTPUT0 -> sum
     {
       std::string out_name("OUTPUT0");
       tsw::Tensor out(out_name);
-      ASSERT_TRUE((err = result.Output(&out)).IsOk()) << err.Message();
+      ASSERT_TRUE((err = result->Output(&out)).IsOk()) << err.Message();
       ASSERT_EQ(out.shape_, std::vector<int64_t>{16});
-      ASSERT_EQ(out.data_type_, tsw::Wrapper_DataType::BYTES);
-      ASSERT_TRUE((err = result.StringData(out_name, &out_str)).IsOk()) << err.Message();
+      ASSERT_EQ(out.data_type_, tsw::DataType::BYTES);
+      ASSERT_TRUE((err = result->StringData(out_name, &out_str)).IsOk())
+          << err.Message();
       for (size_t i = 0; i < input_data.size(); ++i) {
         EXPECT_EQ(out_str[i], std::to_string(2 * input_data[i]));
       }
@@ -221,15 +240,17 @@ TEST_F(TritonServerTest, InferString)
     {
       std::string out_name("OUTPUT1");
       tsw::Tensor out(out_name);
-      ASSERT_TRUE((err = result.Output(&out)).IsOk()) << err.Message();
+      ASSERT_TRUE((err = result->Output(&out)).IsOk()) << err.Message();
       ASSERT_EQ(out.shape_, std::vector<int64_t>{16});
-      ASSERT_EQ(out.data_type_, tsw::Wrapper_DataType::BYTES);
-      ASSERT_TRUE((err = result.StringData(out_name, &out_str)).IsOk()) << err.Message();
+      ASSERT_EQ(out.data_type_, tsw::DataType::BYTES);
+      ASSERT_TRUE((err = result->StringData(out_name, &out_str)).IsOk())
+          << err.Message();
       for (size_t i = 0; i < input_data.size(); ++i) {
         EXPECT_EQ(out_str[i], "0");
       }
     }
-  } catch (...) {
+  }
+  catch (...) {
     ASSERT_NO_THROW(throw);
   }
 }
